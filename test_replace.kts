3ebdcc8e79d8c5c82cd606e7d1e15343d90e529a
pltmustafa <polatmustafa126@gmail.com>
2026-06-15 23:50:39 +0300

    gereksiz dosyaları temizlemece

--- test_replace.kts
+++
@@ -1,4 +0,0 @@
-val extracted = """{\"_id\":\"123\",\"title\":\"Hello \\n World\"}"""
-println("Original: " + extracted)
-val fixed = extracted.replace("\\\"", "\"").replace("\\n", "\n")
-println("Fixed: " + fixed)
