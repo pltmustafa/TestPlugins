c5d02bb5f8f0782495d92f379ab6e8023504e2ed
pltmustafa <polatmustafa126@gmail.com>
2026-06-17 17:19:09 +0300

    beta kekik

--- KekikStream/build.gradle.kts
+++ KekikStream/build.gradle.kts
@@ -5,7 +5,7 @@ cloudstream {
     language    = "tr"
     description = "WatchBuddy api ile film ve dizi izleme eklentisi."
     
-    status  = 1
+    status  = 3
     tvTypes = listOf("Movie", "TvSeries", "Anime")
     iconUrl = "https://watchbuddy.tv/static/home/ico/225x225bb.png"
 }
